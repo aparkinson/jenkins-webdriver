a6c3c79187ad2d9dba62e8c430ffdb2f51503c55
Alan Parkinson <alan.parkinson@storyiq.com>
2012-04-14 17:58:34 +0100

    ChromeDriver added

--- src/test/java/com/storyiq/examples/jenkins/webdriverproxy/JobManagementTest.java
+++ src/test/java/com/storyiq/examples/jenkins/webdriverproxy/JobManagementTest.java
@@ -1,31 +1,67 @@
 package com.storyiq.examples.jenkins.webdriverproxy;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.Proxy;
 import org.openqa.selenium.Proxy.ProxyType;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.PageFactory;
 
 public class JobManagementTest {
 
-	@Test
-    public void testCreateNewJob() {
+    private static ChromeDriverService service;
+    private WebDriver driver;
+
+    @BeforeClass
+    public static void createAndStartService() throws IOException {
+        service = new ChromeDriverService.Builder()
+                .usingChromeDriverExecutable(
+                        new File("C:\\ChromeDriver\\chromedriver.exe"))
+                .usingAnyFreePort().build();
+        service.start();
+    }
+
+    @AfterClass
+    public static void createAndStopService() {
+        service.stop();
+    }
+
+    @Before
+    public void createDriver() {
         Proxy proxy = new Proxy();
         proxy.setAutodetect(false);
         proxy.setProxyType(ProxyType.MANUAL);
         proxy.setHttpProxy("localhost:8080");
 
-        DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
         capabilities.setCapability(CapabilityType.PROXY, proxy);
-		
-        final WebDriver driver = new FirefoxDriver(capabilities);
-		driver.get("http://localhost/jenkins/");		
-		
-		final JenkinsNavigation navigation = PageFactory.initElements(driver, JenkinsNavigation.class);
-		navigation.newJob().createFreeStyleJob("example").setDescription("Hello World");		
-		driver.close();		
-	}
+
+        driver = new RemoteWebDriver(service.getUrl(), capabilities);
+    }
+
+    @After
+    public void quitDriver() {
+        driver.quit();
+    }
+
+    @Test
+    public void testCreateNewJob() {
+        driver.get("http://localhost/jenkins/");
+
+        final JenkinsNavigation navigation = PageFactory.initElements(driver,
+                JenkinsNavigation.class);
+        navigation.newJob().createFreeStyleJob("example")
+                .setDescription("Hello World");
+        driver.close();
+    }
 }
