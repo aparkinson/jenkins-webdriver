a9efdd7e7c29e52d747c55eabc204765d5d2b036
Alan Parkinson <alan.parkinson@storyiq.com>
2012-04-14 15:56:01 +0100

    Use DesiredCapabilities to config proxy settings

--- src/test/java/com/storyiq/examples/jenkins/webdriverproxy/JobManagementTest.java
+++ src/test/java/com/storyiq/examples/jenkins/webdriverproxy/JobManagementTest.java
@@ -1,22 +1,27 @@
 package com.storyiq.examples.jenkins.webdriverproxy;
 
 import org.junit.Test;
+import org.openqa.selenium.Proxy;
+import org.openqa.selenium.Proxy.ProxyType;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.PageFactory;
 
 public class JobManagementTest {
 
 	@Test
-	public void testCreateNewJob() {		
-		final FirefoxProfile profile = new FirefoxProfile();
-		profile.setPreference("network.proxy.type", 1);
-		profile.setPreference("network.proxy.http", "localhost");
-		profile.setPreference("network.proxy.http_port", 8080);
-		profile.setPreference("network.proxy.no_proxies_on", "");		
+    public void testCreateNewJob() {
+        Proxy proxy = new Proxy();
+        proxy.setAutodetect(false);
+        proxy.setProxyType(ProxyType.MANUAL);
+        proxy.setHttpProxy("localhost:8080");
+
+        DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+        capabilities.setCapability(CapabilityType.PROXY, proxy);
 		
-		final WebDriver driver = new FirefoxDriver(profile);		
+        final WebDriver driver = new FirefoxDriver(capabilities);
 		driver.get("http://localhost/jenkins/");		
 		
 		final JenkinsNavigation navigation = PageFactory.initElements(driver, JenkinsNavigation.class);
